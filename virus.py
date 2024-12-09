d9646f941d537b3b4f461e67773c213cc8b82772
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-12-08 20:11:26 -0800

    virus attributes defined

--- virus.py
+++ virus.py
@@ -3,8 +3,8 @@ class Virus(object):
     def __init__(self, name, repro_rate, mortality_rate):
         # Define the attributes of your your virus
         self.name = name
-        # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
 
 # Test this class
@@ -15,3 +15,4 @@ if __name__ == "__main__":
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+    print("All tests passed!")
