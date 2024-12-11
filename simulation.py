958786df260b158cc2cc6f3c4248f9869196b5e4
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-12-11 09:30:03 -0800

    logger works

--- simulation.py
+++ simulation.py
@@ -16,11 +16,12 @@ class Simulation(object):
         self.pop_size = pop_size
         self.vacc_percentage = vacc_percentage
         self.initial_infected = initial_infected
+        self.infected_count = initial_infected
         self.population = self._create_population()
         self.newly_infected = []
-        # self.infected_count = 0
+        
 
-        # print(f"Simulation Initialized. Infected count: {self.infected_count}")
+        print(f"Simulation Initialized. Infected count: {self.infected_count}")
 
         self.logger.write_metadata(
             pop_size, vacc_percentage, virus.name, virus.mortality_rate, virus.repro_rate
@@ -34,7 +35,7 @@ class Simulation(object):
         for i in range(self.pop_size):
             if i < self.initial_infected:
                 population.append(Person(i, is_vaccinated=False, infection=self.virus))
-                # self.infected_count += 1
+                self.infected_count += 1
             else:
                 is_vaccinated = random.random() < self.vacc_percentage
                 population.append(Person(i, is_vaccinated=is_vaccinated))
@@ -86,7 +87,7 @@ class Simulation(object):
         deaths = self.pop_size - survivors
         self.logger.log_final_summary(time_step_counter, self.pop_size, survivors, deaths, vaccinated)
 
-        # self.logger.log_total_infections(self.infected_count)
+        self.logger.log_total_infections(self.infected_count)
 
         clear_log = input("Would you like to clear the log file after this simulation? (y/n): ")
         if clear_log.lower() == 'y':
@@ -106,7 +107,11 @@ class Simulation(object):
         
         if random.random() < self.virus.repro_rate:
             self.newly_infected.append(random_person)
-            # self.infected_count += 1
+            self.infected_count += 1
+
+        if random_person not in self.newly_infected:
+            self.newly_infected.append(random_person)
+
 
     def _infect_newly_infected(self):
         '''
